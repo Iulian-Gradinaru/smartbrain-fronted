ac3129f197e1b8065e499252d62662131d1d2344
IulianGradinaru <iulian_gradinaru@yahoo.com>
2023-01-07 14:57:22 +0200

    change package.json

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-// import React, { Component } from "react";
-import React from "react";
+import React, { Component } from "react";
 import ParticlesBg from "particles-bg";
 import Navigation from "./components/Navigation/Navigation";
 import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Rank from "./components/Rank/Rank";
-
 import "./App.css";
 
 // class App extends Component {
@@ -22,20 +20,40 @@ import "./App.css";
 //   }
 // }
 
-function App() {
-  return (
-    <div className="App">
-      <>
-        <div>...</div>
-        <ParticlesBg type="circle" bg={true} />
-      </>
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      <FaceRecognition />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: "",
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log("click");
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <>
+          <div>...</div>
+          <ParticlesBg type="circle" bg={true} />
+        </>
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition />
+      </div>
+    );
+  }
 }
 
 export default App;
